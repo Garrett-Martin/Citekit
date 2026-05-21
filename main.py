747da3614f59e7b476878506c7f72b55760c350c
Garrett Martin <martingarrettj@gmail.com>
2026-05-21 18:49:39 -0500

    Add menu loop and linked add/view source

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import json
 
-print("Welcome to Citekit")
+def start_menu():
 
-print("1.  Add source manually")
-print("2.  Scrape source from URL")
-print("3.  View saved sources")
-print("4.  Search sources")
-print("5.  Export citation")
-print("6.  Quit")
+    print("Welcome to Citekit")
+
+    print("1.  Add source manually")
+    print("2.  Scrape source from URL")
+    print("3.  View saved sources")
+    print("4.  Search sources")
+    print("5.  Export citation")
+    print("6.  Quit")
+
+    while True:
+        choice = input("Enter option: ")
+
+        if choice == "1":
+            add_source()
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            opens_source()
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            exiting = "Program in exiting"
+            print(exiting)
+            return False
+        else:
+            print("Invalid option, enter 1-6.")
 
 
+sources = []
 
-def create_source():
-    
+def add_source():
     author = input("author: ")
     title = input("title: ")
     publication = input("publication: ")
@@ -21,7 +43,7 @@ def create_source():
     tag = input("tag: ")
     notes = input("notes: ")
 
-    source = {
+    new_source = {
         "author": author,
         "title": title,
         "publication": publication,
@@ -31,18 +53,30 @@ def create_source():
         "notes": notes
     }
 
-    return source
+    try:
+        with open("evidence.json", "r") as file:
+            sources = json.load(file)
+    except FileNotFoundError:
+        sources = []
 
-def save_source(source):
-    with open("source.json", "w") as file:
-        json.dump(source, file, indent=4)
+    sources.append(new_source)
 
+    with open("evidence.json", "w") as file:
+        json.dump(sources, file, indent=4)
+    print("Source added to evidence.json")
+
+
+
+
+def opens_source():
+    with open("evidence.json", "r") as file:
+        sources = json.load(file)
+        for source in sources:
+            print(source)
 
 
 def main():
-    source = create_source()
-    save_source(source)
-    print("Source saved to source.json")
+    start_menu()
 
 
 main()
